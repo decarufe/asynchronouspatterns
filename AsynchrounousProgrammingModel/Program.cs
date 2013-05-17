ae3e9f424ba527166a962d9ddecf56cd61660d49
Eric De Carufel <decarufe@users.noreply.github.com>
2013-05-17 09:40:05 -0400

    Thread and lock branch

---
+++ AsynchrounousProgrammingModel/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using AsyncFramework;
+
+namespace AsynchrounousProgrammingModel
+{
+  static class Program
+  {
+    public static void Main()
+    {
+      // Create an instance of the test class.
+      var asyncDemo = new AsyncDemo();
+
+      var result = asyncDemo.BeginTestMethod(3000);
+
+      Thread.Sleep(10);
+      Console.WriteLine("Main thread {0} does some work.",
+                        Thread.CurrentThread.ManagedThreadId);
+
+      // TODO: Use WaitHandle
+      // TODO: Wait on multiple async calls
+      // TODO: Use polling to wait for completion
+      // TODO: Wait for a short time (less than 2 sec)
+
+      // Call EndInvoke to wait for the asynchronous call to complete, 
+      // and to retrieve the results. 
+      string returnValue = asyncDemo.EndTestMehod(result);
+
+      Console.WriteLine("The call executed with return value \"{0}\".", returnValue);
+    }
+  }
+}
---
+++ ThreadSynchronization/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSynchronization
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var eventWaitHandle = new AutoResetEvent(false);
+      
+      Task.Factory.StartNew(() => DoSometing(1, eventWaitHandle));
+      Thread.Sleep(5);
+      Task.Factory.StartNew(() => DoSometing(2, eventWaitHandle));
+      Thread.Sleep(5);
+      Console.WriteLine("Waiting for enter");
+      Console.ReadLine();
+      eventWaitHandle.Set();
+      Console.ReadLine();
+      eventWaitHandle.Set();
+    }
+
+    static void DoSometing(int id, EventWaitHandle eventWaitHandle)
+    {
+      Console.WriteLine("Starting {0} on thread {1}", id, Thread.CurrentThread.ManagedThreadId);
+      while (!eventWaitHandle.WaitOne(1000))
+      {
+        Console.WriteLine("Waiting {0} on thread {1}", id, Thread.CurrentThread.ManagedThreadId);
+      }
+      Console.WriteLine("Finishing {0} on thread {1}", id, Thread.CurrentThread.ManagedThreadId);
+    }
+  }
+}
