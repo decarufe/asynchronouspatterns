3e741a25abeecbd2ce80ac48fafc255785b05b76
Eric De Carufel <decarufe@users.noreply.github.com>
2013-05-30 12:55:38 -0400

    Task continuation

---
+++ ThreadSynchronization/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSynchronization
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var eventWaitHandle = new AutoResetEvent(false);
+
+      var task = Task.Factory.StartNew(() => DoSometing(1, eventWaitHandle));
+      Thread.Sleep(5);
+      task.ContinueWith(t =>
+      {
+        Task.Factory.StartNew(() => DoSometing(2, eventWaitHandle));
+      }, TaskContinuationOptions.NotOnFaulted);
+      task.ContinueWith(t => Console.WriteLine(t.Exception.InnerExceptions.First().Message), TaskContinuationOptions.OnlyOnFaulted);
+
+      Thread.Sleep(5);
+      Console.WriteLine("Waiting for enter");
+      Console.ReadLine();
+      eventWaitHandle.Set();
+      Console.ReadLine();
+      eventWaitHandle.Set();
+    }
+
+    static void DoSometing(int id, EventWaitHandle eventWaitHandle)
+    {
+      Console.WriteLine("Starting {0} on thread {1}", id, Thread.CurrentThread.ManagedThreadId);
+      while (!eventWaitHandle.WaitOne())
+      {
+        Console.WriteLine("Waiting {0} on thread {1}", id, Thread.CurrentThread.ManagedThreadId);
+      }
+      Console.WriteLine("Finishing {0} on thread {1}", id, Thread.CurrentThread.ManagedThreadId);
+    }
+  }
+}
